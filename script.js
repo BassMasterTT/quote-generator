5414c63215cd3d7bb536bd56b6b605d4d44582fb
BassMasterTT <tothtibi22@gmail.com>
2021-01-01 03:04:54 +0800

    javascript ready

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get Quote From API
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("whoops, no quote", error);
+  }
+}
+
+// On Load
+getQuote();
