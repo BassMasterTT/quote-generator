ec1ca323aa225ee28ce60891f72a8b8c0bb7f35a
BassMasterTT <tothtibi22@gmail.com>
2021-01-02 18:14:59 +0800

    loader working

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 // Get Quote From API
 async function getQuote() {
+  loading();
   const proxyUrl = "https://sheltered-inlet-33387.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -25,6 +41,8 @@ async function getQuote() {
       quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
+    // Stop Loader, Show Quote
+    complete();
   } catch (error) {
     getQuote();
   }
